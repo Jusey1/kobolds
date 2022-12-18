502aa2142ff50ba18c0139052d74bc73715b3134
Salju <61861078+Jusey1@users.noreply.github.com>
2022-12-18 00:39:07 -0500

    Update RascalSpawnProcedure.java

--- src/main/java/net/mcreator/kobolds/procedures/RascalSpawnProcedure.java
+++ src/main/java/net/mcreator/kobolds/procedures/RascalSpawnProcedure.java
@@ -12,7 +12,7 @@ public class RascalSpawnProcedure {
 	public static boolean execute(LevelAccessor world, double x, double y, double z) {
 		boolean Spawn = false;
 		if (!world.isClientSide() && world.isEmptyBlock(new BlockPos(x, y, z))
-				&& world.getBlockState(new BlockPos(x, y, z)).getLightEmission(world, new BlockPos(x, y, z)) <= 5) {
+				&& world.getBlockState(new BlockPos(x, y, z)).getLightEmission(world, new BlockPos(x, y, z)) <= 4) {
 			if (!world.getEntitiesOfClass(KoboldEntity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty()
 					&& !(!world.getEntitiesOfClass(KoboldRascalEntity.class, AABB.ofSize(new Vec3(x, y, z), 128, 128, 128), e -> true).isEmpty())) {
 				Spawn = true;
