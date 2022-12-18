a2e746d1db101c9f12882aaa278dbbf2b32c2c6c
Salju <61861078+Jusey1@users.noreply.github.com>
2022-12-18 00:52:30 -0500

    Update KoboldZombieDrownedEntity.java

--- src/main/java/net/mcreator/kobolds/entity/KoboldZombieDrownedEntity.java
+++ src/main/java/net/mcreator/kobolds/entity/KoboldZombieDrownedEntity.java
@@ -1,12 +1,10 @@
 package net.mcreator.kobolds.entity;
 
 import net.minecraftforge.network.PlayMessages;
-import net.minecraftforge.fml.common.Mod;
 
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Drowned;
@@ -21,7 +19,6 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.GlowSquid;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.DifficultyInstance;
@@ -34,7 +31,6 @@ import net.mcreator.kobolds.init.KoboldsModEntities;
 
 import javax.annotation.Nullable;
 
-@Mod.EventBusSubscriber
 public class KoboldZombieDrownedEntity extends Drowned {
 	public KoboldZombieDrownedEntity(PlayMessages.SpawnEntity packet, Level world) {
 		this(KoboldsModEntities.KOBOLD_ZOMBIE_DROWNED.get(), world);
